02ddfa853946a8301779c0e896633369c0be74ca
aryan-naik-atx <aryannaikofficial@gmail.com>
2025-01-29 20:13:32 +0530

    Added visualisation of output

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
+from matplotlib.ticker import MaxNLocator
+
+
+
+#Note from Aryan : If you want to create more examples then use a 8x8 pixel squal in paint to test
+
+
+
+
 
 def createExamples():
     numberArrayExamples = open('numArEx.txt','a')
@@ -49,7 +58,7 @@ def threshold(imageArray):
                 eachPixel[3] = 255  
     return newArr
 
-
+#funtion to match the number
 def findNum(filePath):
     matchedAr = []
     loadExmpl = open('numArEx.txt','r').read()
@@ -78,14 +87,41 @@ def findNum(filePath):
                     matchedAr.append(int(currentNum))
 
                 x+=1
-    
-    print (matchedAr)
+
 
     x= Counter(matchedAr)
     print (x)
 
+    #attempt to display the number
+    graphX=[]
+    graphY=[]
 
-findNum('imagestest/test2.png')
+    for eachValue in x:
+        print (eachValue)
+        graphX.append(eachValue)
+        print (x[eachValue])
+        graphY.append(x[eachValue])
+
+    gig = plt.figure()
+    ax1 = plt.subplot2grid((4,4),(0,0),rowspan=1,colspan=4)
+    ax2 = plt.subplot2grid((4,4),(1,0),rowspan=3,colspan=4)
+
+    ax1.imshow(iar)
+
+    ax2.bar(graphX,graphY,align='center')
+
+    plt.ylim(400)
+
+    xloc = MaxNLocator(12)
+    ax2.xaxis.set_major_locator(xloc)
+
+    plt.show()
+
+
+
+
+
+findNum('imagestest/test.png')
 
 
 
